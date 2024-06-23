0428bb9f646d82c92754bc598688b9beb1612bed
Kishor272 <kishormahendran6@gmail.com>
2024-06-23 08:56:01 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ const app = express();
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
-const { log } = require('console');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+
 
 const server = http.createServer(app);
 
@@ -20,21 +24,21 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
     console.log(`User connected",${socket.id}`);
 
-     socket.on('join_room',(data)=>{
+    socket.on('join_room', (data) => {
         socket.join(data);
-       console.log(` User with ID:${socket.id} joined room: ${data}`);
-     })
+        console.log(` User with ID:${socket.id} joined room: ${data}`);
+    })
 
-     socket.on('send_message',(data)=>{
-       socket.to(data.room).emit('receive_message',data);
-     })
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit('receive_message', data);
+    })
 
     socket.on('disconnect', () => {
         console.log(`User disconnect", ${socket.id}`);
     })
 })
 
-server.listen(3001, () => {
+server.listen(PORT, () => {
     console.log("server is running");
 })
 
